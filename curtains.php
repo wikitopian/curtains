2f9006fa8f672bb3d8ddb8c6fcfa6a2e759f854d
wikitopian <matt@swarmstrategies.com>
2013-10-05 21:20:49 +0000

    add shortcode

--- curtains.php
+++ curtains.php
@@ -11,12 +11,27 @@ License: GPLv2
 
 class Curtains {
 	private $prefix;
+	private $options;
 
 	public function __construct() {
 		$this->prefix = 'curtains';
 
+		add_action( 'init', array( &$this, 'set_options' ) );
+
 		add_action( 'wp_enqueue_scripts', array( &$this, 'do_style' ) );
 		add_action( 'wp_enqueue_scripts', array( &$this, 'do_script' ) );
+
+		add_shortcode( 'curtains', array( &$this, 'do_shortcode' ) );
+	}
+	public function set_options() {
+		$defaults = array(
+			'description' => '',
+			'width'       => 400,
+			'height'      => 300,
+			'image-left'  => plugins_url( 'images/curtain-left.jpg', __FILE__ ),
+			'image-right' => plugins_url( 'images/curtain-right.jpg', __FILE__ ),
+		);
+		$this->options = get_option( $this->prefix, $defaults );
 	}
 	public function do_style() {
 		wp_enqueue_style(
@@ -33,8 +48,56 @@ class Curtains {
 			true
 		);
 	}
-	public function do_curtains() {
-		include_once 'inc/curtains-template.php';
+	public function do_curtains( $description, $content, $width, $height ) {
+
+		$image_left  = $this->options['image-left'];
+		$image_right = $this->options['image-right'];
+
+		$curtains = <<<HTML
+
+<!--START THE WRAPPER-->
+<div class='curtain_wrapper'>
+
+	<!-- 2 CURTAIN IMAGES START HERE  -->
+	<img class='curtain curtainLeft'   src='{$image_left}' />
+	<img class='curtain curtainRight'  src='{$image_right}' />
+    <!-- END IMAGES -->
+
+    <!-- START THE CONTENT DIV -->
+	<div class='content'>
+		<!-- YOUR CONTENT HERE -->
+		{$content}
+	<!-- END YOUR CONTENT -->
+    </div>
+    <!-- END THE CONTENT DIV -->
+
+    <!-- START DESCRIPTION DIV, WHICH WILL BE SHOWN ON TOP OF THE CURTAIN AND REMOVED WHEN THE CURTAINS OPEN -->
+    <div class='description'>
+		{$description}
+    </div>
+    <!-- END DESCRIPTION DIV -->
+
+</div>
+<!--END THE WRAPPER-->
+
+HTML;
+		return $curtains;
+	}
+	public function do_shortcode( $atts, $content = '' ) {
+		do_shortcode( $content ); // recursion
+
+		extract(
+			shortcode_atts(
+				array(
+					'description' => $this->options['description'],
+					'width'       => $this->options['width'],
+					'height'      => $this->options['height'],
+				),
+				$atts
+			)
+		);
+
+		return $this->do_curtains( $description, $content, $width, $height );
 	}
 }
 $curtains = new Curtains();
