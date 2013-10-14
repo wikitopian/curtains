cdd2fc551871aa886152607ee98bdf8670ca9131
wikitopian <matt@swarmstrategies.com>
2013-10-14 14:54:43 +0000

    minify jq

--- js/curtains.js
+++ js/curtains.js
@@ -1,13 +1,14 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function( $ ){
+
     // when user clicks inside the container...
-    jQuery('.curtain_wrapper').click(function(){
+    $('.curtain_wrapper').click(function(){
         // animate the description div by changing it's left position to it's width (but as negative number)...
-        jQuery(this).children('.description').animate({'left': -1*jQuery(this).width()});
+        $(this).children('.description').animate({'left': -1*$(this).width()});
 
         // animate the 2 curtain images to width of 50px with duration of 2 seconds...
-        jQuery(this).children('img.curtain').animate({ width: 50 },{duration: 2000});
+        $(this).children('img.curtain').animate({ width: 50 },{duration: 2000});
 
         // show the content behind the curtains with fadeIn function (2 seconds)
-        jQuery(this).children('.content').fadeIn(2000);
+        $(this).children('.content').fadeIn(2000);
     });
 });
