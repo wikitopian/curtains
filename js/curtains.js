f7512a0d23e40fcc959e8c28a00ebd5e22b97c24
wikitopian <matt@swarmstrategies.com>
2013-10-14 18:04:09 +0000

    fix jq dim problems

--- js/curtains.js
+++ js/curtains.js
@@ -3,12 +3,12 @@ jQuery(document).ready(function( $ ){
     // when user clicks inside the container...
     $('.curtain_wrapper').click(function(){
         // animate the description div by changing it's left position to it's width (but as negative number)...
-        $(this).children('.description').animate({'left': -1*$(this).width()});
+        $(this).children('.curtain_description').animate({'left': -1*$(this).width()});
 
         // animate the 2 curtain images to width of 50px with duration of 2 seconds...
         $(this).children('img.curtain').animate({ width: 0 },{duration: 2000});
 
         // show the content behind the curtains with fadeIn function (2 seconds)
-        $(this).children('.content').fadeIn(2000);
+        $(this).children('.curtain_content').fadeIn(2000);
     });
 });
