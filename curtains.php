7e1264815dd04f988f7f5306c02969cd48ec34c7
wikitopian <matt@swarmstrategies.com>
2013-10-14 17:59:51 +0000

    dimensions work now

--- curtains.php
+++ curtains.php
@@ -48,7 +48,51 @@ class Curtains {
 			true
 		);
 	}
-	public function do_curtains( $description, $content, $width, $height ) {
+	public function do_shortcode( $atts, $content = '' ) {
+		do_shortcode( $content ); // recursion
+
+		extract(
+			shortcode_atts(
+				array(
+					'description' => $this->options['description'],
+					'width'       => $this->options['width'],
+					'height'      => $this->options['height'],
+				),
+				$atts
+			)
+		);
+
+		$this->do_dimensions( $width, $height );
+
+		return $this->do_curtains( $description, $content );
+	}
+	public function do_dimensions( $width, $height ) {
+
+		$css = <<<CSS
+
+<style>
+
+img.curtain {
+    height: {$height}px;
+}
+
+.curtain_wrapper {
+	width:  {$width}px;
+	height: {$height}px;
+}
+
+.curtain_content {
+	width: {$width}px;
+}
+
+</style>
+
+CSS;
+
+		echo $css;
+
+	}
+	public function do_curtains( $description, $content ) {
 
 		$image_left  = $this->options['image-left'];
 		$image_right = $this->options['image-right'];
@@ -64,7 +108,7 @@ class Curtains {
     <!-- END IMAGES -->
 
     <!-- START THE CONTENT DIV -->
-	<div class='content'>
+	<div class='curtain_content'>
 		<!-- YOUR CONTENT HERE -->
 		{$content}
 	<!-- END YOUR CONTENT -->
@@ -72,7 +116,7 @@ class Curtains {
     <!-- END THE CONTENT DIV -->
 
     <!-- START DESCRIPTION DIV, WHICH WILL BE SHOWN ON TOP OF THE CURTAIN AND REMOVED WHEN THE CURTAINS OPEN -->
-    <div class='description'>
+    <div class='curtain_description'>
 		{$description}
     </div>
     <!-- END DESCRIPTION DIV -->
@@ -82,22 +126,6 @@ class Curtains {
 
 HTML;
 		return $curtains;
-	}
-	public function do_shortcode( $atts, $content = '' ) {
-		do_shortcode( $content ); // recursion
-
-		extract(
-			shortcode_atts(
-				array(
-					'description' => $this->options['description'],
-					'width'       => $this->options['width'],
-					'height'      => $this->options['height'],
-				),
-				$atts
-			)
-		);
-
-		return $this->do_curtains( $description, $content, $width, $height );
 	}
 }
 $curtains = new Curtains();
